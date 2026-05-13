d155c4ad724fc4f55d0ad68297b4fe669874283e
Milad <miladk.f14@gmail.com>
2026-05-13 11:21:16 +0200

    Update ID types to int; add Repositories folder to project
    Changed MenuItem.Id and OrderItem.MenuId from string to int, updating constructors accordingly. Added "Repositories" folder to Chapeau.csproj for project structure.

--- Chapeau/Models/MenuItem.cs
+++ Chapeau/Models/MenuItem.cs
@@ -2,7 +2,7 @@ namespace Chapeau.Models
 {
     public class MenuItem
     {
-        public string Id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string Category { get; set; }
@@ -13,7 +13,7 @@ namespace Chapeau.Models
 
         public MenuItem() { }
 
-        public MenuItem(string id, string name, string description, string category, decimal price, int vat, string allergens, bool inStock)
+        public MenuItem(int id, string name, string description, string category, decimal price, int vat, string allergens, bool inStock)
         {
             Id = id;
             Name = name;
--- Chapeau/Models/OrderItem.cs
+++ Chapeau/Models/OrderItem.cs
@@ -4,7 +4,7 @@ namespace Chapeau.Models
     {
         public int Id { get; set; }
         public int OrderId { get; set; }
-        public string MenuId { get; set; }
+        public int MenuId { get; set; }
         public string Name { get; set; }
         public int Qty { get; set; }
         public decimal Price { get; set; }
@@ -13,7 +13,7 @@ namespace Chapeau.Models
 
         public OrderItem() { }
 
-        public OrderItem(int id, int orderId, string menuId, string name, int qty, decimal price, int vat, string itemType)
+        public OrderItem(int id, int orderId, int menuId, string name, int qty, decimal price, int vat, string itemType)
         {
             Id = id;
             OrderId = orderId;
