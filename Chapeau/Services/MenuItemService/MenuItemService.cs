56282b5c978803ebfb5860ca02e130ce93e6f245
Milad <miladk.f14@gmail.com>
2026-05-17 22:29:28 +0200

    Refactor MenuItemService to use block-bodied methods
    Converted all expression-bodied members in MenuItemService to block-bodied methods with curly braces for improved readability and consistency. No functional changes were made.

--- Chapeau/Services/MenuItemService/MenuItemService.cs
+++ Chapeau/Services/MenuItemService/MenuItemService.cs
@@ -12,12 +12,39 @@ namespace Chapeau.Services
             _repository = repository;
         }
 
-        public List<MenuItem> GetAll() => _repository.GetAll();
-        public List<MenuItem> GetByCategory(string category) => _repository.GetByCategory(category);
-        public MenuItem GetById(int id) => _repository.GetById(id);
-        public void Add(MenuItem item) => _repository.Add(item);
-        public void Update(MenuItem item) => _repository.Update(item);
-        public void Delete(int id) => _repository.Delete(id);
-        public void UpdateStock(int id, bool inStock) => _repository.UpdateStock(id, inStock);
+        public List<MenuItem> GetAll()
+        {
+            return _repository.GetAll();
+        }
+
+        public List<MenuItem> GetByCategory(string category)
+        {
+            return _repository.GetByCategory(category);
+        }
+
+        public MenuItem GetById(int id)
+        {
+            return _repository.GetById(id);
+        }
+
+        public void Add(MenuItem item)
+        {
+            _repository.Add(item);
+        }
+
+        public void Update(MenuItem item)
+        {
+            _repository.Update(item);
+        }
+
+        public void Delete(int id)
+        {
+            _repository.Delete(id);
+        }
+
+        public void UpdateStock(int id, bool inStock)
+        {
+            _repository.UpdateStock(id, inStock);
+        }
     }
 }
