9566f0bce2f8aeee1c98d2b0b3c44d35299796c9
BringMeChaos <38015593+BringMeChaos@users.noreply.github.com>
2026-06-05 10:01:38 +0200

    created basic view and controller with updated group code

---
+++ Chapeau/Controllers/OrderController.cs
@@ -0,0 +1,45 @@
+﻿using Chapeau.Models;
+using Microsoft.AspNetCore.Mvc;
+using Chapeau.Services;
+using Chapeau.ViewModels;
+
+namespace Chapeau.Controllers
+{
+    public class OrderController : Controller
+    {
+
+        private readonly IOrderService _orderService;
+        private readonly IMenuItemService _menuItemService;
+        private readonly IStaffService _staffService;
+        private readonly IRestaurantTableService _tableService;
+
+        public OrderController(IOrderService orderService, IMenuItemService menuItemService, IStaffService staffService, IRestaurantTableService tableService)
+        {
+            _menuItemService = menuItemService;
+            _orderService = orderService;
+            _staffService = staffService;
+            _tableService = tableService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateOrder(int tableId) // int staffId
+        {
+            RestaurantTable tableOrder = _tableService.GetTableById(tableId);
+
+            List<OrderItem> orderItems = new List<OrderItem>();
+
+            List<MenuItem> menuItems = _menuItemService.GetAllMenuItems();
+
+            Staff staff = _staffService.GetStaffById(2); // staffId
+
+            OrderViewModel orderViewModel = new OrderViewModel(menuItems, orderItems, tableOrder, staff);
+
+
+            return View("CreateOrder", orderViewModel);
+        }
+    }
+}
---
+++ Chapeau/ViewModels/OrderViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Chapeau.Models;
+
+namespace Chapeau.ViewModels
+{
+    public class OrderViewModel
+    {
+        public List<MenuItem> MenuItems;
+        public List<OrderItem> OrderItems;
+        public RestaurantTable TableOrder; // need to address table number 
+        public Staff StaffOrder;
+
+
+        public OrderViewModel(List<MenuItem> menuItems, List<OrderItem> orderItems, RestaurantTable tableOrder, Staff staffOrder)
+        {
+            MenuItems = menuItems;
+            OrderItems = orderItems;
+            TableOrder = tableOrder;
+            StaffOrder = staffOrder;
+        }
+    }
+}
+
